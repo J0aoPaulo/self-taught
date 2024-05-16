3924cacb31377b3d52960f58252cf4618b721def
J0aoPaulo <ojoaopaulo1324@gmail.com>
2024-05-16 00:55:21 -0300

    api with filewriter

--- Backend/api-test/api-consumption/2944-java-screenmatch-consumindo-webservice/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ Backend/api-test/api-consumption/2944-java-screenmatch-consumindo-webservice/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -6,37 +6,62 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner input = new Scanner(System.in);
-        System.out.println("Busque pelo nome do filme: ");
-        String movieName = input.nextLine();
-
-        var endereco = "http://www.omdbapi.com/?t=" + movieName + "&apikey=31542b50";
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-
-        String json = response.body();
-        System.out.println(json);
-
+        String busca = "";
+        List<Titulo> titulos = new ArrayList<>();
         Gson gson = new GsonBuilder()
                 .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
                 .create();
 
-        TituloOmdb tituloOmdb = gson.fromJson(json, TituloOmdb.class);
-        Titulo meuTitulo = new Titulo(tituloOmdb);
-        System.out.println(meuTitulo);
+        while ((!busca.equalsIgnoreCase("sair"))) {
+            System.out.println("Busque por um filme: ");
+            busca = input.nextLine();
+
+            if (busca.equalsIgnoreCase("sair")) break;
+
+            String endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=31542b50";
+
+            try {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(endereco))
+                        .build();
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+
+                String json = response.body();
+                System.out.println(json);
+
+                TituloOmdb tituloOmdb = gson.fromJson(json, TituloOmdb.class);
+                Titulo meuTitulo = new Titulo(tituloOmdb);
+                System.out.println(meuTitulo);
+
+                titulos.add(meuTitulo);
+            } catch (NumberFormatException e) {
+                System.out.println("Aconteceu um erro: ");
+                System.out.println(e.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Algum erro de argumento na busca, verifique o endereço");
+            }
+        }
+        System.out.println(titulos);
+
+        FileWriter escrita = new FileWriter("filmes.json");
+        escrita.write(gson.toJson(titulos));
+        escrita.close();
+        System.out.println("O programa finalizou corretamente!");
     }
 }
---
+++ Backend/api-test/api-consumption/viacep-api/src/Main.java
@@ -0,0 +1,57 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner input = new Scanner(System.in);
+        List<String> ceps = new ArrayList<>();
+        List<String> cepsJson = new ArrayList<>();
+
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+
+        while (true) {
+            System.out.print("Digite um cep: ");
+            String cep = input.nextLine();
+
+            if (cep.equalsIgnoreCase("sair")) break;
+            while (cep.length() != 8) {
+                System.out.print("Por favor, digite um cep valido: ");
+                cep = input.nextLine();
+            }
+            ceps.add(cep);
+        }
+        for (String cep : ceps) {
+            String endereco = "https://viacep.com.br/ws/" + cep + "/json/";
+            try {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(endereco))
+                        .build();
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+
+                cepsJson.add(response.body());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        FileWriter fileWriter = new FileWriter("ceps-residenciais.json");
+        for (String endereco : cepsJson) {
+            fileWriter.write(endereco);
+        }
+        fileWriter.close();
+        System.out.println("Arquivo criado com sucesso");
+    }
+}
